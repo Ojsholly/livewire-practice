742a6c6e5e52b70b07663a4e2567d1ba9d6ce471
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2021-03-31 03:06:51 +0100

    Modify trusted proxy info

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 
-        if (config('APP_ENV') == 'production') {
+        if (config('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
 
