f6854264079576982f6c43f57450cffecaa1e78d
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2021-04-05 05:22:17 +0100

    Added contact form design, validation, and logic.

--- app/Http/Livewire/Forms/ContactForm.php
+++ app/Http/Livewire/Forms/ContactForm.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Livewire\Forms;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class ContactForm extends Component
 {
+    public $name;
+    public $email;
+    public $message;
+    public $success;
+
+    protected $rules = [
+        'name' => 'required|string',
+        'email' => 'required|email',
+        'message' => 'required|min:5'
+    ];
+
+    public function updated($field)
+    {
+        $this->validateOnly($field);
+    }
+
+    public function submit()
+    {
+
+        Contact::create($this->validate());
+
+        $this->reset(['name', 'email', 'message']);
+
+        $this->success = 'Contact Form Successfully Submitted';
+    }
+
     public function render()
     {
         return view('livewire.forms.contact-form');
